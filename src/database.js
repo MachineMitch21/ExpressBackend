d77c2ef5b96a5b7dd8ac0254e3105057a1c9e4e6
MachineMitch21 <mitchschutt@outlook.com>
2019-04-27 12:18:01 -0400

    Initialization of Database object state with tests

--- src/database.js
+++ src/database.js
@@ -1,25 +1,32 @@
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
 
-const Database = function(url, options) {
+const Database = function(url, options, connectionCb) {
 	this.con = null;
 	this.url = url;
 
-	let _options = Object.assign({ 
-		waitConnect: false,
-		persistConnection: true,
-	}, options);
+	let _options = Object.assign(
+		{
+			waitConnect: false,
+			persistConnection: true
+		},
+		options
+	);
 
-	if (_options.waitConnect !== undefined && _options.waitConnect === false) {
-		this.connect();
+	// if persistConnection is false, waitConnect is ignored
+	if (_options.persistConnection === true) {
+		if (_options.waitConnect !== undefined && _options.waitConnect === false) {
+			this.connect({}, connectionCb);
+		}
 	}
 };
 
-
 Database.prototype.connect = function(options, connectionCb) {
-
-	let _options = Object.assign({
-		overrideConnection: false
-	}, options);
+	let _options = Object.assign(
+		{
+			overrideConnection: false
+		},
+		options
+	);
 
 	if (this.con === null || (this.con !== null && _options.overrideConnection === true)) {
 		MongoClient.connect(
@@ -45,13 +52,17 @@ Database.prototype.connect = function(options, connectionCb) {
 	let _this = this;
 };
 
-Database.prototype.disconnect = function() {
-	
-	if (this.db !== undefined && this.db instanceof MongoClient) {
-
-	} else {
-
+Database.prototype.disconnect = function(disconnectCb) {
+	if (this.con !== undefined && this.con instanceof MongoClient) {
+		this.con.close();
+		this.con = null;
 	}
+
+	if (typeof disconnectCb === 'function') disconnectCb(this);
+};
+
+Database.prototype.hasValidConnection = function() {
+	return this.con instanceof MongoClient;
 }
 
 export default Database;
--- test/database.spec.js
+++ test/database.spec.js
@@ -1,10 +1,26 @@
 import assert from 'assert';
 import Database from '../src/database';
 
+const url = 'mongodb://localhost:27017/test_db';
+
 it('database wait connect', () => {
-	let db = new Database('mongodb://localhost:27017/test_db', {
+	let db = new Database(url, {
 		waitConnect: true
 	});
 
 	assert(true, db.con === null);
+});
+
+it('database connect on initialization', () => {
+	let db = new Database(url, {
+		waitConnect: false
+	});
+
+	assert(true, db.hasValidConnection());
+});
+
+it('database connectionCallback returns Database object', () => {
+	let db = new Database(url, {}, (dbObj) => {
+		assert(true, db === dbObj);
+	});
 });
